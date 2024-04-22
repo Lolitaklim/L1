e8dc75091a820343d94213664d2401598b0a7e2f
Lolitaklim <lolita.klim@yandex.com>
2024-04-22 12:27:33 +0400

    upd

--- tasks19,20/task19.666.js
+++ tasks19,20/task19.666.js
@@ -11,160 +11,119 @@ let count = 10;
 // смещение (не берем первй пост закрепленный)
 let offset = 1;
 
-
+// счетчик колбэков
 let callbackCount = 0;
+
+// получение данных из localStorage
 let storedPosts = JSON.parse(localStorage.getItem('posts')) || [];
 
-let isFetching = false;
+let downloadStorage = false;
 
-let isInitial = false;
+// получаем посты
+async function fetchPosts() {
+    
+    // создаем скрипт для получения данных (протокол JSONP)
+    // удалённый сервер должен в ответ сгенерировать скрипт, который вызывает currentCallback() с данными, которые хочет передать.
+    const script = document.createElement('script');
+    // имя нашей функции – в URL-параметре callback
+    const currentCallback = `jsonpCallback${callbackCount}`;
+    // добавляем скрипт в head
+    script.src = `${apiUrl}&owner_id=${ownerId}&access_token=${accessToken}&offset=${offset}&count=${count}&v=5.199&callback=${currentCallback}`;
 
-function jsonpRequest (currentCallback, script) {
-    return new Promise((resolve, reject) => {
+    // Создаем Promise, который будет разрешен при успешной загрузке данных
+    const dataPromise = new Promise((resolve, reject) => {
+        // функция, которая вызывается при получении данных
         window[currentCallback] = function (data) {
             resolve(data);
             delete window[currentCallback];
         };
+        // обработка ошибки
         script.onerror = function () {
             reject(new Error('JSONP request failed'));
             delete window[currentCallback];
         };
     });
-}
-
-// получаем посты
-async function fetchPosts() {
-
-    const script = document.createElement('script');
-    const currentCallback = `jsonpCallback${callbackCount}`;
-    script.src = `${apiUrl}&owner_id=${ownerId}&access_token=${accessToken}&offset=${offset}&count=${count}&v=5.199&callback=${currentCallback}`;
 
     // добавляем скрипт в head
     document.head.appendChild(script);
     callbackCount++;
 
     // ожидаем разрешения Promise
-    const data = await jsonpRequest (currentCallback, script);
+    const data = await dataPromise;
 
     // удаление скрипта
     document.head.removeChild(script);
 
-    // сортировка по дате по возрастанию, в начале старые посты потом новые
-    return data.response.items.sort((a, b) => a.date - b.date);
+    return data.response.items;
 }
 
 async function downloadPosts() {
+
     const posts = await fetchPosts(); 
 
-    // // увеличение смещения
-    // offset += count;
-
-
-    // добавление новых постов в конец массива 
-    storedPosts = [...storedPosts, ...posts];
-    
-
-    appendPostsToWidget(posts);
-    savePostsToLocalStorage(storedPosts);
     // увеличение смещения
     offset += count;
 
-    isFetching = false;
-}
+    // сортировка по дате по возрастанию
+    // const posts = postsNoSort.slice().sort((a, b) => a.date - b.date);
+    posts.sort((a, b) => a.date - b.date);
 
-function formatDateFromMs(ms) {
-    let date = new Date(ms * 1000)
-    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-\
-${date.getDate().toString().padStart(2, '0')} \
-${date.getHours().toString().padStart(2, '0')}:\
-${date.getMinutes().toString().padStart(2, '0')}:\
-${date.getSeconds().toString().padStart(2, '0')}`
+    // добавление новых постов в конец массива 
+    storedPosts = [...storedPosts, ...posts];
+
+    // добавление постов в виджет
+    appendPostsToWidget(posts);
+    savePostsToLocalStorage(storedPosts);
+
+    // task20  вывод объем занятой памяти / максимальный размер хранилища в мб
+    console.log(`${(calculateLocalStorageSize() / (1024 * 1024)).toFixed(6)} MB / ${fillLocalStorageToMax} MB`);
+
+    
 }
 
 // первоначальная загрузка постов из учета localStorage
-async function initialDownload() {
+async function initialDownload(storedPosts) {
 
     const posts = await fetchPosts();
 
-    // storedPosts.sort((a, b) => a.date - b.date);
+    for(let i = 0; i < posts.length; i++) {
+        console.log(posts[i].id, storedPosts[0].id);
+        if (posts[i].id === storedPosts[0].id) {
+            posts.splice(0, i);
+            // storedPosts = [...posts, ...storedPosts];
+            storedPosts.splice(offset, 0, ...posts);
 
-    // storedPosts.forEach((post, index) => {
-    //     console.log(formatDateFromMs(post.date), index);
-
-    // })
-
-    console.log(posts);
-
-    for (let i = posts.length - 1; i > 0; i--) {
-        
-    
-    // posts.forEach((post, index) => { 
-
-        console.log("Downloaded:",posts[i].id, formatDateFromMs(posts[i].date), "Local:", storedPosts[storedPosts.length - 1].id, formatDateFromMs(storedPosts[storedPosts.length - 1].date));
-
-       
-        if (posts[i].id === storedPosts[storedPosts.length - 1].id) {
-            console.log('нашлось сравнение');
-
-            if(!posts) {
-                appendPostsToWidget(storedPosts);
-                offset = storedPosts.lenght + 1;
-
-                isInitial = false;
-
-                return;
-            } 
-            // удаляем все оставшееся вкл индекс
-            posts.splice(i);
-            // isInitial = false;
-            console.log(posts);
-
-            if(!posts) return;
-            appendPostsToWidget(posts);
-            offset += posts.lenght;
-            appendPostsToWidget(storedPosts);
-
-            storedPosts = [...posts, ...storedPosts];
-
-            offset = storedPosts.lenght + 1;
-        //     storedPosts.splice(offset, 0, ...posts);
-
-        //     appendPostsToWidget(storedPosts);// доб v nachalo!
+            appendPostsToWidget(storedPosts);// доб v nachalo!
             savePostsToLocalStorage(storedPosts);
-            isInitial = false;
-            isFetching = false;
-        //     offset = storedPosts.length + 1;
-        //     isInitial = false;
+            console.log(`${(calculateLocalStorageSize() / (1024 * 1024)).toFixed(6)} MB / ${fillLocalStorageToMax} MB`)
+
+            offset = storedPosts.length + 1;
+            downloadStorage = false;
             return;
         }
-    
-    // })
-
     }
 
-    storedPosts = [...posts, ...storedPosts]; // posle offsetnstored posts stored
+    // storedPosts = [...posts, ...storedPosts]; // posle offsetnstored posts stored
+    storedPosts.splice(offset, 0, ...posts);
    
-   
-    appendPostsToWidget(posts);// доб v nachalo!
-    console.log('нашлось');
+    appendPostsToWidget(storedPosts);// доб v nachalo!
 
-    // savePostsToLocalStorage(storedPosts);
-   
+    savePostsToLocalStorage(storedPosts);
+    console.log(`${(calculateLocalStorageSize() / (1024 * 1024)).toFixed(6)} MB / ${fillLocalStorageToMax} MB`)
+
     offset += count;
-
-    
-    isFetching = false;
+    // appendPostsToWidget(storedPosts);
+    // offset += storedPosts.length;
+    // initialDownload(storedPosts);
 }
 
 // функция для сохранения постов в localStorage
 function savePostsToLocalStorage(storedPosts) {    
     try {
-        storedPosts.sort((a, b) => a.date - b.date);
         localStorage.setItem('posts', JSON.stringify(storedPosts));
-        console.log(`${(calculateLocalStorageSize() / (1024 * 1024)).toFixed(6)} MB / ${fillLocalStorageToMax} MB`);
     } catch (e) {
-        // если произошла ошибка, удаляем первые count постов
-        storedPosts.splice(0, count);
+        // если произошла ошибка, удаляем первые 10 постов
+        storedPosts.splice(0, 10);
         savePostsToLocalStorage(storedPosts);
     }
 }
@@ -175,10 +134,14 @@ const postList = document.getElementById('post-list');
 // добавление постов в виджет
 function appendPostsToWidget(posts) {
 
+    // проходимся по каждому посту
     posts.forEach(post => {
+
+        // создаем элемент списка
         const listItem = document.createElement('li');
         listItem.classList.add('post');
 
+        // создаем элементы для отображения даты
         const date = new Date(post.date * 1000); 
         const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
         const dateItem = document.createElement('span');
@@ -186,32 +149,42 @@ function appendPostsToWidget(posts) {
         listItem.appendChild(dateItem);
         dateItem.textContent = formattedDate;
 
+        // создаем элемент для отображения текста поста
         const textItem = document.createElement('span');
         textItem.classList.add('text');
         listItem.appendChild(textItem);
         textItem.textContent = post.text;        
+ 
+        // если есть вложения и длина вложений больше 0
         if (post.attachments && post.attachments.length > 0) {
+
+            // получаем фото
             const photoAttachment = post.attachments[0].photo;
+            
+            // если есть фото и размеры фото
             if (photoAttachment && photoAttachment.sizes && photoAttachment.sizes.length > 0) {
+                // получаем размеры фото
                 const sizes = photoAttachment.sizes;
+                // получаем последний размер фото, url фото
                 const image = sizes[sizes.length - 1].url;
+                // создаем элемент img
                 const imgElement = document.createElement('img');
                 imgElement.src = image;
                 listItem.appendChild(imgElement);
             }
         }
 
+        // добавляем элемент в список
         // добавляем элемент в список на определенную позицию (offset)
-        postList.insertBefore(listItem, postList.children[offset - 1]);
+        postList.insertBefore(listItem, postList.children[offset - count - 1]);
+        // postList.appendChild(listItem);
     });
 }
 
 // первоначальная загрузка данных из localStorage при открытии страницы
 addEventListener('load', function () {
     if (storedPosts.length > 0) {
-        isFetching = true;
-        isInitial = true;
-        initialDownload();
+        initialDownload(storedPosts);
     } else {
         downloadPosts();
     }
@@ -224,9 +197,7 @@ postsWidget.addEventListener('scroll', function () {
     const clientHeight = postsWidget.clientHeight;
 
     // если скролл внизу страницы (осталось 10 пикселей) - загружаем новые посты
-    if (scrollTop + clientHeight >= scrollHeight - 10 && isFetching === false) {
-        isInitial ?  initialDownload() : downloadPosts();
-        isFetching = true;
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+        downloadPosts();
     }
 });
-
